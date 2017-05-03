0a9a05d968ab245d68d15e8f288bddcb7310ed4a
Whois API LLC <support@whoisxmlapi.com>
2017-05-03 22:12:42 +0300

    Update .NET sample to UTC

--- assets/samples/key/WhoisApiKeyAuth.cs
+++ assets/samples/key/WhoisApiKeyAuth.cs
@@ -11,8 +11,8 @@ class Program {
     public const string username = "your whois api username";
 
     static void Main() {
-        long time = (long)(DateTime.Now.Subtract(new DateTime(1970, 1, 1))
-                            .TotalMilliseconds * 1000);
+        long time = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))
+                            .TotalMilliseconds);
         string reqData='\u007B'+$"\"t\":{time},\"u\":\"{username}\""+'\u007D';
         var reqBytes = Encoding.UTF8.GetBytes(reqData);
         string req = Convert.ToBase64String(reqBytes);
