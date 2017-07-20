762e3b0d66618e78c522bb362812da47c402b76b
Hongyi Bian <hobian95@gmail.com>
2017-07-19 22:02:45 -0700

    changed

--- apikey/java/src/main/java/com/whoisxmlapi/test/ApiKeyClientTest.java
+++ apikey/java/src/main/java/com/whoisxmlapi/test/ApiKeyClientTest.java
@@ -1,5 +1,7 @@
 package com.whoisxmlapi.test;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URLEncoder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,6 +23,7 @@ public class ApiKeyClientTest {
     
 	public static void main(String[]args) {
 	    new ApiKeyClientTest().getSimpleDomainUsingApiKey();
+	    
 	}
 
 	private void getSimpleDomainUsingApiKey() {
@@ -40,7 +43,16 @@ public class ApiKeyClientTest {
         String res = null;
         try {
             c.executeMethod(m);
-            res = new String(m.getResponseBody());
+                        
+            BufferedReader reader = new BufferedReader(new InputStreamReader(m.getResponseBodyAsStream()));  
+            StringBuffer stringBuffer = new StringBuffer();  
+            String str = "";  
+            while((str = reader.readLine())!=null){  
+               stringBuffer.append(str + "\n");  
+            }  
+            res = stringBuffer.toString();  
+            
+            //res = new String(m.getResponseBody());
         } catch (Exception e) {
             logger.log(Level.SEVERE, "Cannot get url", e);
         } finally {
